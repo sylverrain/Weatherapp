828f778752e6bcd7f26fb8af6d97c7821b364dd2
sylve <sylverrain@gmail.com>
2023-04-08 23:51:04 +0300

    Weatherapp ver 2

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -95,6 +95,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             TextView speed=findViewById(R.id.windSpeed);
             speed.setText(wind.getString("speed"));
 
+            TextView humidity=findViewById(R.id.humidityValue);
+            humidity.setText(weather.getString("humidity"));
+
             TextView sunrise=findViewById(R.id.timeSunrise);
             String timeSunrise=sys.getString("sunrise");
             Locale myLocale=new Locale("ru", "RU");
